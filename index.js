0e97c316b5ca38969d9f33686216728eaba0551e
Robert A <daspanisharmada@gmail.com>
2019-10-01 23:28:01 -0700

    toggle hamburger/close icon + light nav

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const toggle = (evt, feature) => {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(feature).style.display = "grid";
-    evt.currentTarget.className += " active";
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(feature).style.display = "grid";
+  evt.currentTarget.className += " active";
+}
 
-  document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
 
-const dropdown = () => document.getElementById("myDropdown").classList.toggle("show");
+const dropdown = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("hamburger-icon").classList.toggle("hide");
+  document.getElementById("close-icon").classList.toggle("show");
+
+  document.getElementById("logo").classList.toggle("hide");
+
+}
